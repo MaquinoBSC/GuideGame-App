f16a3325760e43a332081a750747a9d54ff579bd
MaquinoBSC <martinare.ipn@gmail.com>
2021-10-19 19:56:45 -0500

    Implementar funcion para logear usuarios

--- scripts/auth.js
+++ scripts/auth.js
@@ -29,3 +29,23 @@ logout.addEventListener('click', (e)=>{
         console.log("User Signed Out");
     });
 });
+
+
+//Login
+const loginForm= document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e)=> {
+    e.preventDefault();
+
+    //get user Info
+    const email= loginForm["login-email"].value;
+    const password= loginForm["login-password"].value;
+
+    auth.signInWithEmailAndPassword(email, password).then((cred)=> {
+        console.log(cred.user);
+
+        //close the login modal and reset the form
+        const modal= document.querySelector('#modal-signin');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+    });
+});
